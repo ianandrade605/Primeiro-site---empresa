e15e15864f23f99cb972f932bc9aa7241129eaee
pazeevedo <p.azemelgon@gmail.com>
2026-04-05 19:34:32 -0300

    Add function to display cart notification

--- site empresa/script.js
+++ site empresa/script.js
@@ -2,3 +2,12 @@ function enviarFormulario(event) {
     event.preventDefault();
     alert("Mensagem enviada com sucesso!");
 }
+function adicionarCarrinho() {
+    var aviso = document.getElementById("aviso-carrinho");
+
+    aviso.style.display = "block";
+
+    setTimeout(function() {
+        aviso.style.display = "none";
+    }, 2000);
+}
